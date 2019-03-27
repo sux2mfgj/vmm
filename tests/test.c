f8edb6d5c4d9477f766459a26bfe63a57b23588c
Shunsuke Mie <sux2mfgj@gmail.com>
2019-03-27 15:24:47 +0900

    apply clang-format

--- tests/test.c
+++ tests/test.c
@@ -8,27 +8,27 @@
 
 static void open_dev_kvm(void)
 {
-        int fd = open("/dev/kvm", O_RDWR);
-        assert(fd >= 0 && "failed to open /dev/kvm");
-        close(fd);
+	int fd = open("/dev/kvm", O_RDWR);
+	assert(fd >= 0 && "failed to open /dev/kvm");
+	close(fd);
 }
 
 static void check_api_version(void)
 {
-        int fd = open("/dev/kvm", O_RDWR);
-        assert(fd >= 0 && "failed to open /dev/kvm");
+	int fd = open("/dev/kvm", O_RDWR);
+	assert(fd >= 0 && "failed to open /dev/kvm");
 
-        int api_version = ioctl(fd, KVM_GET_API_VERSION, 0);
-        assert(api_version == KVM_API_VERSION);
+	int api_version = ioctl(fd, KVM_GET_API_VERSION, 0);
+	assert(api_version == KVM_API_VERSION);
 
-        close(fd);
+	close(fd);
 }
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
         open_dev_kvm();
-        check_api_version();
+	check_api_version();
 
-        printf("no error detected\n");
-        return 0;
+	printf("no error detected\n");
+	return 0;
 }
