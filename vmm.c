dc531aa0a7c7b0db9462a4f4d780555c1d0bd510
Shunsuke Mie <sux2mfgj@gmail.com>
2019-03-27 09:50:55 +0900

    Create a interface at /dev/kvm

--- vmm.c
+++ vmm.c
@@ -1,17 +1,85 @@
 #include <linux/module.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Shunsuke Mie <sux2mfgj@gmail.com>");
 
-static int vmm_init(void)
+#define DEVICE_NAME "kvm"
+#define CLASS_NAME "vmm"
+
+static int majorNumber;
+static struct class* vmm_class = NULL;
+static struct device* vmm_device = NULL;
+
+static long vmm_dev_ioctl(struct file* filep,
+                unsigned int ioctl,
+                unsigned long arg)
 {
-        printk("vmm: Hello, world\n");
+       long r = -EINVAL;
+       return r;
+}
+static struct file_operations vmm_fops = {.unlocked_ioctl = vmm_dev_ioctl};
+
+static int register_device(void)
+{
+        int r;
+        majorNumber = register_chrdev(0, DEVICE_NAME, &vmm_fops);
+        if(majorNumber < 0)
+        {
+                printk(KERN_ALERT "vmm: failed to register a major number\n");
+                r = majorNumber;
+                goto failed;
+        }
+
+        vmm_class = class_create(THIS_MODULE, CLASS_NAME);
+        if (IS_ERR(vmm_class))
+        {
+                r = PTR_ERR(vmm_class);
+                goto failed_class_create;
+        }
+
+        vmm_device = device_create(vmm_class, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+        if(IS_ERR(vmm_device))
+        {
+                r = PTR_ERR(vmm_device);
+                goto failed_create_device;
+        }
 
         return 0;
+
+failed_create_device:
+        class_unregister(vmm_class);
+        class_destroy(vmm_class);
+failed_class_create:
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+failed:
+        return r;
+}
+
+static void unregister_device(void)
+{
+
+        device_destroy(vmm_class, MKDEV(majorNumber, 0));
+        class_unregister(vmm_class);
+        class_destroy(vmm_class);
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+}
+
+static int vmm_init(void)
+{
+        int r = 0;
+        printk("vmm: Hello, world\n");
+
+        r = register_device();
+
+        return r;
 }
 
 static void vmm_exit(void)
 {
+        unregister_device();
         printk("vmm: byebye\n");
 }
 
