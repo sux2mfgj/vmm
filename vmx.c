3173c8c5cd938d09203090abc10ea9b1216bd33d
Shunsuke Mie <sux2mfgj@gmail.com>
2019-03-27 14:35:31 +0900

    this patch be able to vmxon and vmxoff.

--- vmx.c
+++ vmx.c
@@ -48,32 +48,50 @@ static struct vmcs* alloc_vmcs_region(void)
 static int vmxon(uint64_t address)
 {
         uint32_t cr4;
-        //uint64_t rflags;
+        uint64_t rflags, is_error;
 
         // enable virtual machine extension
         asm volatile ("mov %%cr4, %0" : "=r"(cr4));
-        cr4 |= X86_CR4_VME;
+        cr4 |= X86_CR4_VMXE;
         asm volatile ("mov %0, %%cr4" : : "r"(cr4));
 
-        //TODO
+        // vmxon
+        asm volatile ("vmxon %0" : : "m"(address));
+
+        // check the result
+        asm volatile ("pushfq\n\t"
+                      "pop %0" : "=g"(rflags));
+        is_error = rflags & X86_EFLAGS_CF;
+
+        return is_error;
+}
+
+static int vmxoff(void)
+{
+        asm volatile ("vmxoff");
         return 0;
 }
 
 int vmx_setup(void)
 {
         int r = 0;
+        uint64_t vmxon_region_pa;
+
         vmxon_region = alloc_vmcs_region();
         if(!vmxon_region)
         {
                 return -ENOMEM;
         }
+        vmxon_region_pa = __pa(vmxon_region);
 
-        r = vmxon(__pa(vmxon_region));
+        printk("0x%llx 0x%llx\n", (uint64_t)vmxon_region, vmxon_region_pa);
+        r = vmxon(vmxon_region_pa);
 
         return r;
 }
 
 void vmx_tear_down(void)
 {
-
+        vmxoff();
+        free_page((unsigned long)vmxon_region);
 }
